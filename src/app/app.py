a3814df274be7532f779f32d523d3d5b06cd9f8d
Marik28 <markel20002000@gmail.com>
2022-02-11 16:48:47 +0600

    register persons blueprint

--- src/app/app.py
+++ src/app/app.py
@@ -3,4 +3,8 @@ from flask import Flask
 
 def create_app() -> Flask:
     app = Flask(__name__)
+
+    from .blueprints import persons
+    app.register_blueprint(persons.blueprint, url_prefix="/persons")
+
     return app
