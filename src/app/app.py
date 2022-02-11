911be468276b22a7e4fdb06b09ceae133bbe3e8e
mark <markel20002000@gmail.com>
2022-02-11 21:44:21 +0600

    add shows blueprint

--- src/app/app.py
+++ src/app/app.py
@@ -4,6 +4,9 @@ from flask import Flask
 def create_app() -> Flask:
     app = Flask(__name__)
 
+    from .blueprints import shows
+    app.register_blueprint(shows.blueprint, url_prefix="/shows")
+
     from .blueprints import persons
     app.register_blueprint(persons.blueprint, url_prefix="/persons")
 
--- src/app/blueprints/shows.py
+++ src/app/blueprints/shows.py
@@ -3,5 +3,27 @@ from flask import (
     render_template,
 )
 
+from ..database import Session
+from ..services.shows import ShowsService
 
-blueprint = Blueprint('persons', __name__, template_folder='templates')
+blueprint = Blueprint('shows', __name__, template_folder='templates')
+
+
+@blueprint.get("/")
+def get_shows():
+    with Session() as session:
+        shows_service = ShowsService(session)
+        page = shows_service.get_page(1, 10)
+        context = {
+            "shows": page.items,
+            "page": page,
+        }
+        return render_template("shows/list.html", **context)
+
+
+@blueprint.get("/<show_id>")
+def get_show(show_id):
+    with Session() as session:
+        service = ShowsService(session)
+        show = service.get(show_id)
+        return render_template("shows/detail.html", show=show)
