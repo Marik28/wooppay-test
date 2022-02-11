88c254af6e1213253e80bae9ed980d6b2d227530
mark <markel20002000@gmail.com>
2022-02-12 00:58:14 +0600

    override methods

--- src/app/services/persons.py
+++ src/app/services/persons.py
@@ -1,3 +1,10 @@
+from typing import Optional
+
+from sqlalchemy.orm import (
+    Query,
+    joinedload,
+)
+
 from .generic import GenericCRUDService
 from .. import tables
 
@@ -5,3 +12,18 @@ from .. import tables
 class PersonsService(GenericCRUDService):
     model = tables.Person
     order_by = tables.Person.fullname.asc()
+
+    def get_list_query(self, *, search: Optional[str] = None) -> Query:
+        query = self.get_default_query()
+        if search:
+            query = query.filter(tables.Person.fullname.ilike(f"%{search}%"))
+        return query
+
+    def get_detail_query(self) -> Query:
+        return (
+            self.get_default_query()
+                .options(
+                joinedload(tables.Person.directed).load_only(tables.Show.title, tables.Show.show_id),
+                joinedload(tables.Person.starred_in).load_only(tables.Show.title, tables.Show.show_id),
+            )
+        )
