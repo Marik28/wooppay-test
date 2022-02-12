6f2e2788d0f79729ca2fff808e94b6b2df5d3cda
mark <markel20002000@gmail.com>
2022-02-13 02:53:54 +0600

    add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+
+from app.app import create_app
+from app.database import Session
+from app.services.shows import ShowsService
+
+
+@pytest.fixture(scope="function")
+def test_client():
+    app = create_app()
+    client = app.test_client()
+
+    return client
+
+
+@pytest.fixture(scope="function")
+def session():
+    db_session = Session()
+    try:
+        yield db_session
+    finally:
+        db_session.close()
+
+
+@pytest.fixture(scope="function")
+def shows_service(session):
+    return ShowsService(session)
