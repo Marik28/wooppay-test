5e3d85797fef65f8161d25c473d4fa8a3a32d446
mark <markel20002000@gmail.com>
2022-02-12 17:31:29 +0600

    add checkbox_list.py

---
+++ src/app/forms/fields/checkbox_list.py
@@ -0,0 +1,10 @@
+from wtforms import SelectMultipleField
+from wtforms.widgets import (
+    ListWidget,
+    CheckboxInput,
+)
+
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = ListWidget(prefix_label=False)
+    option_widget = CheckboxInput()
