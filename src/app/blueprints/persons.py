63c380765af0538b890e938cb519c61427163270
mark <markel20002000@gmail.com>
2022-02-12 17:44:42 +0600

    change view

--- src/app/blueprints/persons.py
+++ src/app/blueprints/persons.py
@@ -3,7 +3,6 @@ from flask import (
     render_template, request,
 )
 
-from ..app import csrf
 from ..database import Session
 from ..forms.persons import PersonsSearchForm
 from ..services.persons import PersonsService
@@ -11,14 +10,14 @@ from ..services.persons import PersonsService
 blueprint = Blueprint('persons', __name__, template_folder='templates')
 
 
+# TODO: add reset filters button
 @blueprint.get("/")
-@csrf.exempt
 def get_persons():
     form = PersonsSearchForm(request.args)
     if form.validate():
         with Session() as session:
             service = PersonsService(session)
-            page = service.get_page(form.page.data, form.per_page.data, search=form.search.data)
+            page = service.get_page(**form.data)
             persons = page.items
     else:
         persons = []
