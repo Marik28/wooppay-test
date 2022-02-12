82d6c16d565f4b79e4a0c07697cdcea171bbf528
mark <markel20002000@gmail.com>
2022-02-12 17:47:30 +0600

    change view

--- src/app/blueprints/shows.py
+++ src/app/blueprints/shows.py
@@ -1,22 +1,43 @@
 from flask import (
     Blueprint,
     render_template,
+    request,
 )
 
 from ..database import Session
+from ..forms.shows import ShowsSearchForm
+from ..services.countries import CountriesService
+from ..services.genres import GenresService
+from ..services.ratings import RatingsService
 from ..services.shows import ShowsService
+from ..utils.models import to_list_of_strings
 
 blueprint = Blueprint('shows', __name__, template_folder='templates')
 
 
+# TODO: add reset filters button
+
 @blueprint.get("/")
 def get_shows():
     with Session() as session:
-        shows_service = ShowsService(session)
-        page = shows_service.get_page(1, 10)
+        form = ShowsSearchForm(request.args, active=True)
+        available_ratings = RatingsService(session).get_list()
+        form.rating.choices = to_list_of_strings(available_ratings, "code")
+        available_countries = CountriesService(session).get_list()
+        form.countries.choices = to_list_of_strings(available_countries, "name")
+        available_genres = GenresService(session).get_list()
+        form.genres.choices = to_list_of_strings(available_genres, "name")
+        if form.validate():
+            shows_service = ShowsService(session)
+            page = shows_service.get_page(**form.data)
+            shows = page.items
+        else:
+            page = None
+            shows = []
     context = {
-        "shows": page.items,
+        "shows": shows,
         "page": page,
+        "form": form,
     }
     return render_template("shows/list.html", **context)
 
