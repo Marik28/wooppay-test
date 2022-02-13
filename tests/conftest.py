5673807203080ec0b13e29b26050112bfc5c8438
mark <markel20002000@gmail.com>
2022-02-13 14:58:46 +0600

    add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 
 from app.app import create_app
 from app.database import Session
+from app.services.genres import GenresService
 from app.services.shows import ShowsService
 
 
@@ -25,3 +26,8 @@ def session():
 @pytest.fixture(scope="function")
 def shows_service(session):
     return ShowsService(session)
+
+
+@pytest.fixture(scope="function")
+def genres_service(session):
+    return GenresService(session)
